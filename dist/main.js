abe26fa81e9298297116b92cb505cbd67c6c72c7
Martins Cudars <martins.cudars@gmail.com>
2022-07-12 02:03:08 +0300

    add market view

--- dist/main.js
+++ dist/main.js
@@ -161,7 +161,7 @@ const $18c53b0039ffc5db$export$c1975daa4eb91b44 = (bestPotential)=>{
         playerRow.appendChild((0, $18c53b0039ffc5db$export$b36ad6a61166502b)(bestSkillWithExp, `${rowClass}td2`));
         const ratingTd = document.createElement("td");
         ratingTd.classList.add(`${rowClass}td1`);
-        ratingTd.appendChild((0, $18c53b0039ffc5db$export$83fab2b954b58590)(bestPosition.skill));
+        ratingTd.appendChild((0, $18c53b0039ffc5db$export$83fab2b954b58590)(bestSkillWithExp));
         playerRow.appendChild(ratingTd);
     });
 };
@@ -201,7 +201,7 @@ const $95930220612465e5$var$viewPlayerProfile = ()=>{
     content.appendChild(skill);
     const comparison = document.createElement("div");
     comparison.classList.add("comparison");
-    comparison.appendChild((0, $18c53b0039ffc5db$export$83fab2b954b58590)(bestPosition.skill));
+    comparison.appendChild((0, $18c53b0039ffc5db$export$83fab2b954b58590)((0, $78fc06ffa0ef6332$export$5898f23eb7acb0be)(bestPosition.skill, player.experience)));
     content.appendChild(comparison);
     const bestPotential = (0, $78fc06ffa0ef6332$export$82338cb6413791b1)((0, $78fc06ffa0ef6332$export$bf339f9dce5a47df)(player));
     const potential = (0, $18c53b0039ffc5db$export$c1975daa4eb91b44)(bestPotential);
@@ -238,11 +238,55 @@ const $95930220612465e5$var$viewLineupChange = ()=>{
         playerRow.appendChild((0, $18c53b0039ffc5db$export$b36ad6a61166502b)((0, $78fc06ffa0ef6332$export$5898f23eb7acb0be)(bestPosition.skill, player.experience), `${rowClass}td2`));
     });
 };
+const $95930220612465e5$var$viewMarket = ()=>{
+    const tableHeads = document.getElementById("table-1").querySelectorAll("thead");
+    const playerRows = document.getElementById("table-1").querySelector("tbody").querySelectorAll("tr");
+    tableHeads.forEach((head)=>{
+        head.querySelector("tr").appendChild((0, $18c53b0039ffc5db$export$b36ad6a61166502b)("POS", "th1"));
+        head.querySelector("tr").appendChild((0, $18c53b0039ffc5db$export$b36ad6a61166502b)("SK", "th2"));
+        head.querySelector("tr").appendChild((0, $18c53b0039ffc5db$export$b36ad6a61166502b)("RATING", "th1"));
+    });
+    const getSkill = (column)=>{
+        return parseInt([].reduce.call(column.childNodes, (a, b)=>{
+            return a + (b.nodeType === 3 ? b.textContent : "");
+        }, ""));
+    };
+    playerRows.forEach((playerRow, index)=>{
+        const playerColumns = playerRow.querySelectorAll("td");
+        const player = {
+            name: playerColumns[0].textContent,
+            age: playerColumns[1].textContent,
+            careerLongitivity: Array.from(playerColumns[4].textContent)[0],
+            skills: {
+                goalie: getSkill(playerColumns[5]),
+                defence: getSkill(playerColumns[6]),
+                offence: getSkill(playerColumns[7]),
+                shooting: getSkill(playerColumns[8]),
+                passing: getSkill(playerColumns[9]),
+                technical: getSkill(playerColumns[10]),
+                aggression: getSkill(playerColumns[11])
+            },
+            experience: parseInt(playerColumns[12].textContent),
+            overall: playerColumns[13].textContent
+        };
+        const rowClass = index % 2 === 0 ? "tr1" : "tr0";
+        const skills = (0, $78fc06ffa0ef6332$export$f424e510a287eb0)(player);
+        const bestPosition = (0, $78fc06ffa0ef6332$export$fefc44fbabdf230f)(skills);
+        const bestSkillWithExp = (0, $78fc06ffa0ef6332$export$5898f23eb7acb0be)(bestPosition.skill, player.experience);
+        playerRow.appendChild((0, $18c53b0039ffc5db$export$b36ad6a61166502b)(bestPosition.position, `${rowClass}td1`));
+        playerRow.appendChild((0, $18c53b0039ffc5db$export$b36ad6a61166502b)(bestSkillWithExp, `${rowClass}td2`));
+        const ratingTd = document.createElement("td");
+        ratingTd.classList.add(`${rowClass}td1`);
+        ratingTd.appendChild((0, $18c53b0039ffc5db$export$83fab2b954b58590)(bestSkillWithExp));
+        playerRow.appendChild(ratingTd);
+    });
+};
 /**
  * Run View Functions
  */ if (window.location.href.includes("speletaju-parskats")) $95930220612465e5$var$viewPlayerList();
 if (window.location.href.includes("speletajs")) $95930220612465e5$var$viewPlayerProfile();
 if (window.location.href.includes("rediget-mainu")) $95930220612465e5$var$viewLineupChange();
+if (window.location.href.includes("/lv/tirgus")) $95930220612465e5$var$viewMarket();
 
 
 //# sourceMappingURL=main.js.map
--- src/main.js
+++ src/main.js
@@ -67,7 +67,7 @@ const viewPlayerList = () => {
 
     const ratingTd = document.createElement("td");
     ratingTd.classList.add(`${rowClass}td1`);
-    ratingTd.appendChild(renderComparison(bestPosition.skill));
+    ratingTd.appendChild(renderComparison(bestSkillWithExp));
 
     playerRow.appendChild(ratingTd);
   });
@@ -131,7 +131,11 @@ const viewPlayerProfile = () => {
 
   const comparison = document.createElement("div");
   comparison.classList.add("comparison");
-  comparison.appendChild(renderComparison(bestPosition.skill));
+  comparison.appendChild(
+    renderComparison(
+      calculateSkillWithExp(bestPosition.skill, player.experience)
+    )
+  );
   content.appendChild(comparison);
 
   const bestPotential = calculateBestPotential(
@@ -193,6 +197,76 @@ const viewLineupChange = () => {
   });
 };
 
+const viewMarket = () => {
+  const tableHeads = document
+    .getElementById("table-1")
+    .querySelectorAll("thead");
+
+  const playerRows = document
+    .getElementById("table-1")
+    .querySelector("tbody")
+    .querySelectorAll("tr");
+
+  tableHeads.forEach((head) => {
+    head.querySelector("tr").appendChild(renderTableCell("POS", "th1"));
+    head.querySelector("tr").appendChild(renderTableCell("SK", "th2"));
+    head.querySelector("tr").appendChild(renderTableCell("RATING", "th1"));
+  });
+
+  const getSkill = (column) => {
+    return parseInt(
+      [].reduce.call(
+        column.childNodes,
+        (a, b) => {
+          return a + (b.nodeType === 3 ? b.textContent : "");
+        },
+        ""
+      )
+    );
+  };
+
+  playerRows.forEach((playerRow, index) => {
+    const playerColumns = playerRow.querySelectorAll("td");
+
+    const player = {
+      name: playerColumns[0].textContent,
+      age: playerColumns[1].textContent,
+      careerLongitivity: Array.from(playerColumns[4].textContent)[0],
+      skills: {
+        goalie: getSkill(playerColumns[5]),
+        defence: getSkill(playerColumns[6]),
+        offence: getSkill(playerColumns[7]),
+        shooting: getSkill(playerColumns[8]),
+        passing: getSkill(playerColumns[9]),
+        technical: getSkill(playerColumns[10]),
+        aggression: getSkill(playerColumns[11]),
+      },
+      experience: parseInt(playerColumns[12].textContent),
+      overall: playerColumns[13].textContent,
+    };
+
+    const rowClass = index % 2 === 0 ? "tr1" : "tr0";
+    const skills = calculatePositionsSkills(player);
+    const bestPosition = calculateBestPosition(skills);
+    const bestSkillWithExp = calculateSkillWithExp(
+      bestPosition.skill,
+      player.experience
+    );
+
+    playerRow.appendChild(
+      renderTableCell(bestPosition.position, `${rowClass}td1`)
+    );
+
+    playerRow.appendChild(renderTableCell(bestSkillWithExp, `${rowClass}td2`));
+
+    const ratingTd = document.createElement("td");
+    ratingTd.classList.add(`${rowClass}td1`);
+    ratingTd.appendChild(renderComparison(bestSkillWithExp));
+
+    playerRow.appendChild(ratingTd);
+  });
+};
+
 /**
  * Run View Functions
  */
@@ -202,3 +276,5 @@ if (window.location.href.includes("speletaju-parskats")) viewPlayerList();
 if (window.location.href.includes("speletajs")) viewPlayerProfile();
 
 if (window.location.href.includes("rediget-mainu")) viewLineupChange();
+
+if (window.location.href.includes("/lv/tirgus")) viewMarket();
