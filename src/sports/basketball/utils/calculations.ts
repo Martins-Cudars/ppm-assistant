68d3840d60f7aa79e4e508914ea0397d82b1c093
Martins Cudars <martins.cudars@gmail.com>
2026-04-10 16:36:54 +0300

    remove old unused code

--- src/sports/basketball/utils/calculations.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Basketball-specific calculation utilities
- * Placeholder for future basketball-specific calculations
- * Basketball currently doesn't use growth prediction utils
- */
-
-// Future basketball calculations will go here
